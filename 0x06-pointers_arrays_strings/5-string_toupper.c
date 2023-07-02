f86d0ca0e684cae936bb20942f83194736c0d3a6
MrsRams2 <t.ramuhashi@gmail.com>
2023-07-02 10:43:21 -0700

    function that changes all lowercase letters

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -1,13 +1,12 @@
 #include "main.h"
 
 /**
- * string_toupper - 'function that changes all lowercase letters'
- * @s: input string
- *
- * Return: n
- */
-
-char *string_toupper(char *)
+* string_toupper - change all lowercase to uppercase
+* @n: pointer
+**
+Return: n
+*/
+char *string_toupper(char *n)
 {
 int i;
 i = 0;
@@ -16,6 +15,6 @@ while (n[i] != '\0')
 if (n[i] >= 'a' && n[i] <= 'z')
 n[i] = n[i] - 32;
 i++;
-}
+} 
 return (n);
 }
