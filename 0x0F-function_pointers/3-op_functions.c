89d05d81b16522fc99bfd03ded866d9df2827df7
MrsRams2 <t.ramuhashi@gmail.com>
2023-07-30 09:06:12 -0700

    program that performs simple operations

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -15,7 +15,7 @@ int op_mod(int a, int b);
 int op_add(int a, int b)
 {
 return (a + b);
-} 
+}
 /**
  * op_sub - Returns the difference of two numbers.
  * @a: The first number.
@@ -26,7 +26,7 @@ return (a + b);
 int op_sub(int a, int b)
 {
 return (a - b);
-} 
+}
 
 /**
  * op_mul - Returns the product of two numbers.
@@ -38,7 +38,7 @@ return (a - b);
 int op_mul(int a, int b)
 {
 return (a * b);
-} 
+}
 
 /**
  * op_div - Returns the division of two numbers.
@@ -50,7 +50,7 @@ return (a * b);
 int op_div(int a, int b)
 {
 return (a / b);
-} 
+}
 
 /**
  * op_mod - Returns the remainder of the division of two numbers.
