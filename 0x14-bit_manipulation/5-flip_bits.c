15a9397ca29a063a14b5a9c206118c626b3cc46b
MrsRams2 <t.ramuhashi@gmail.com>
2023-09-01 17:56:22 -0700

    new update task 5

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -2,7 +2,7 @@
 
 /**
  * flip_bits - function that returns the number of bits
- * @n: number to be returned
+ * @n: first number to be returned
  * @m: second number to be returned
  *
  * Return: number of bits
@@ -10,16 +10,16 @@
 
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
-int k, count = 0;
-unsigned long int first;
-unsigned long int second = n ^ m;
+	int k, count = 0;
+	unsigned long int current;
+	unsigned long int another = n ^ m;
 
-for (k = 1024; k >= 0; k--)
-{
-first = second >> k;
-if (first & 1)
-count++;
-}
+	for (k = 98; k >= 0; k--)
+	{
+		current = another >> k;
+		if (current & 1)
+			count++;
+	}
 
-return (count);
+	return (count);
 }
